ebd9c350624ce0f70cbe4b1e234b91be8a79829f
Mesut Tasci <mst.tasci@gmail.com>
2023-01-01 04:35:42 +0300

    move commands different package

---
+++ commands/cli.go
@@ -0,0 +1,38 @@
+package commands
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/urfave/cli/v2"
+)
+
+func App() *cli.App {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatalf("user home directory path getting failed: %v", err)
+	}
+
+	return &cli.App{
+		Usage: "git activity mirror",
+		Flags: []cli.Flag{
+			&cli.PathFlag{
+				Name:  "repo",
+				Value: fmt.Sprintf(filepath.Join(homeDir, ".git-mirror")),
+				Usage: "git repo directory path of the mirror repo",
+			},
+			&cli.StringFlag{
+				Name:    "whitelist",
+				Value:   "",
+				Usage:   "comma seperated file extensions to create stats. eg: go,rs,sh,Makefile",
+				EnvVars: []string{"GIT_MIRROR_FILE_TYPE_WHITELIST"},
+			},
+		},
+		Commands: []*cli.Command{
+			InstallCmd,
+			AddCmd,
+		},
+	}
+}
