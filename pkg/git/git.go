02ad4aa6c05744f88e45f1ce99aeb992d5925f1b
Mesut Tasci <mst.tasci@gmail.com>
2023-01-08 22:49:28 +0300

    handle adding stats of empty or 1 commit repo

--- pkg/git/git.go
+++ pkg/git/git.go
@@ -1,11 +1,18 @@
 package git
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strconv"
+	"strings"
 )
+
+// Empty tree hash
+// https://github.com/git/git/blob/d9a3764af74ac215e06543c263ec21196d672b49/cache.h#L1027-L1028
+const EmptyTreeHash = "4b825dc642cb6eb9a060e54bf8d69288fbee4904"
 
 func ValidateRepo(path string) error {
 	if f, err := os.Stat(filepath.Join(path, ".git")); err != nil {
@@ -29,22 +36,71 @@ func AddChanges(repoPath string) error {
 	return nil
 }
 
-func Commit(repoPath, msg string) error {
-	_, err := exec.Command("git", "-C", repoPath, "commit", "-m", msg).Output()
+func Commit(path, msg string) error {
+	_, err := exec.Command("git", "-C", path, "commit", "-m", msg).Output()
 	if err != nil {
-		return fmt.Errorf("`git -C %s commit -m '%s'` command execution failed. error %v", repoPath, msg, err)
+		return fmt.Errorf("`git -C %s commit -m '%s'` command execution failed. error %v", path, msg, err)
 	}
 
 	return nil
 }
 
-func LastCommitStatsDiff(repoPath string) ([]byte, error) {
-	out, err := exec.Command("git", "-C", repoPath, "diff", "--numstat", "HEAD~1").Output()
+func LastCommitStatsDiff(path string) ([]byte, error) {
+	revision, err := findDiffRevision(path)
 	if err != nil {
-		return nil, fmt.Errorf("`git -C %s diff --numstat HEAD~1` command execution failed with error: `%v`", repoPath, err)
+		return nil, err
+	}
+
+	return numStatDiff(path, revision)
+}
+
+func numStatDiff(path, revision string) ([]byte, error) {
+	out, err := exec.Command("git", "-C", path, "diff", "--numstat", revision).Output()
+	if err != nil {
+		return nil, fmt.Errorf("`git -C %s diff --numstat %s` command execution failed with error: `%v`", path, revision, err)
 	}
 
 	return out, nil
+}
+
+func findDiffRevision(path string) (string, error) {
+	c, err := commitCount(path)
+	if err != nil {
+		return "", err
+	}
+
+	if c > 1 {
+		return "HEAD~1", nil
+	} else if c == 1 {
+		return EmptyTreeHash, nil
+	}
+
+	return "", fmt.Errorf("git repo needs to have at least one commit")
+}
+
+// commitCount returns num 0, 1 or 2
+func commitCount(path string) (int, error) {
+	var out []byte
+	var err error
+	rev := "HEAD"
+
+	// We don't need to count all commits, so we are giving --max-count=2
+	out, err = exec.Command("git", "-C", path, "rev-list", "--count", "--max-count=2", rev).Output()
+	if err != nil {
+		// commit count less than 2
+		// so checking there is any commit
+		out, err = exec.Command("git", "-C", path, "rev-list", "--count", "--max-count=2", EmptyTreeHash).Output()
+		if err != nil {
+			return 0, errors.New("git commit count getting failed")
+		}
+	}
+
+	c, err := strconv.Atoi(strings.Replace(string(out), "\n", "", -1))
+	if err != nil {
+		return 0, fmt.Errorf("commit count parse failed")
+	}
+
+	return c, nil
 }
 
 // AddHook adds given content to the given hook file
@@ -74,7 +130,7 @@ func createHookDir(path string) error {
 	hookDir := filepath.Join(path, ".git", "hooks")
 	if _, err := os.Stat(hookDir); os.IsNotExist(err) {
 		if err := os.MkdirAll(hookDir, os.ModeDir); err != nil {
-			return fmt.Errorf(hookDir+" directory creation failed: %v", err)
+			return fmt.Errorf("%s directory creation failed: %v", hookDir, err)
 		}
 	}
 
