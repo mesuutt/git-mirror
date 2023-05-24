10904c5f878cc8fb1aa1fbf2bbab04b7f332080e
Mesut Tasci <mst.tasci@gmail.com>
2023-05-25 02:18:43 +0300

    add test for adding new diff in same day

--- pkg/repo/fsrepo_test.go
+++ pkg/repo/fsrepo_test.go
@@ -20,13 +20,13 @@ func TestRepo_AddChange(t *testing.T) {
 		changes []commit.Change
 	}{
 		{
-			name: "one commit",
+			name: "one file changed",
 			changes: []commit.Change{
 				{Insertion: 1, Deletion: 2, Filename: "log.go", Dir: "2023/01/01", Text: "1 insertion(s), 2 deletion(s)"},
 			},
 		},
 		{
-			name: "two commit",
+			name: "two file changed",
 			changes: []commit.Change{
 				{Insertion: 3, Deletion: 4, Filename: "log.rs", Dir: "2023/01/02", Text: "3 insertion(s), 4 deletion(s)"},
 				{Insertion: 2, Deletion: 3, Filename: "log.go", Dir: "2023/01/02", Text: "2 insertion(s), 3 deletion(s)"},
@@ -63,4 +63,36 @@ func TestRepo_AddChange(t *testing.T) {
 
 		})
 	}
+}
+
+func TestRepo_AddChange_In_Same_Day(t *testing.T) {
+	repoPath, _ := os.MkdirTemp("", "")
+	t.Cleanup(func() { os.RemoveAll(repoPath) })
+	repo := NewFsRepo(repoPath)
+
+	dir := "2023/01/04"
+	filename := "log.go"
+	diff1 := commit.Diff{Changes: []commit.Change{{Insertion: 1, Deletion: 2, Filename: filename, Dir: dir, Text: "content1"}}}
+	diff2 := commit.Diff{Changes: []commit.Change{{Insertion: 3, Deletion: 4, Filename: filename, Dir: dir, Text: "content2"}}}
+
+	if err := repo.AddStats(diff1); err != nil {
+		t.Errorf("stat add failed: %v", err)
+	}
+
+	if err := repo.AddStats(diff2); err != nil {
+		t.Errorf("stat add failed: %v", err)
+	}
+
+	path := filepath.Join(repoPath, dir, filename)
+	if _, err := os.Stat(path); err != nil {
+		t.Fatalf("expected stat file not found in repo: %s", path)
+	}
+
+	content, _ := os.ReadFile(path)
+
+	expectedContent := fmt.Sprintf("%s\n", "content1\ncontent2")
+	if string(content) != expectedContent {
+		t.Fatalf("file content not mathed. want: `%s`, got: `%s`", expectedContent, string(content))
+	}
+
 }
